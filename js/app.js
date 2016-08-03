96afdb04ba6228075cfefdd404af10fa298bc702
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-08-03 16:02:39 -0600

    added pointArray to app.ts

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
-(function () {
-    'use strict';
-    var Point = (function () {
-        function Point(x, y) {
-            this.x = x;
-            this.y = y;
-        }
-        Point.prototype.add = function (point) {
-            return new Point(this.x + point.x, this.y + point.y);
-        };
-        Point.prototype.toString = function () {
-            return this.x + " " + this.y;
-        };
-        return Point;
-    }());
-    var p1 = new Point(10, 30);
-    var p2 = new Point(3, 9);
-    var p3 = p1.add(p2);
-    document.write(p3.toString());
-});
+var Point = (function () {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Point.prototype.add = function (point) {
+        return new Point(this.x + point.x, this.y + point.y);
+    };
+    Point.prototype.toString = function () {
+        return this.x + " " + this.y;
+    };
+    return Point;
+}());
+var p1 = new Point(1, 2);
+var p2 = new Point(10, 20);
+var p3 = new Point(5, 6);
+var p4 = new Point(7, 3);
+var pointArray = new Array();
+pointArray.push(p1);
+pointArray.push(p2);
+pointArray.push(p3);
+pointArray.push(p4);
+for (var i = 0; i < pointArray.length; i++) {
+    console.log(pointArray[i].toString());
+}
--- js/app.ts
+++ js/app.ts
@@ -1,30 +1,36 @@
-(function() {
-  'use strict';
+class Point {
+  private x: number;
+  private y: number;
 
-  class Point {
-    x: number;
-    y: number;
-
-    constructor(x: number, y: number) {
-      this.x = x;
-      this.y = y;
-    }
-
-    add(point: Point) : Point {
-      return new Point(
-        this.x + point.x,
-        this.y + point.y
-      );
-    }
-
-    toString() : string {
-      return this.x + " " + this.y;
-    }
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
   }
 
-  let p1 = new Point(10, 30);
-  let p2 = new Point(3 , 9 );
-  let p3 = p1.add(p2);
+  add(point: Point) : Point {
+    return new Point(
+      this.x + point.x,
+      this.y + point.y
+    );
+  }
 
-  document.write(p3.toString());
-});
+  toString() : string {
+    return this.x + " " + this.y;
+  }
+}
+
+var p1 = new Point(1, 2);
+var p2 = new Point(10, 20);
+var p3 = new Point(5, 6);
+var p4 = new Point(7, 3);
+
+let pointArray: Array<Point> = new Array();
+
+pointArray.push(p1);
+pointArray.push(p2);
+pointArray.push(p3);
+pointArray.push(p4);
+
+for (let i: number = 0; i < pointArray.length; i++) {
+  console.log(pointArray[i].toString());
+}
