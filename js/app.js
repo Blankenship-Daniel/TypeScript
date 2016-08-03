9e507844482b133758d735359a2a19c79c35b3ac
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-08-03 15:34:34 -0600

    newest commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
-var Point = (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    Point.prototype.add = function (point) {
-        return new Point(this.x = Point.x, this.y = Point.y);
-    };
-    return Point;
-}());
+(function () {
+    'use strict';
+    var Point = (function () {
+        function Point(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+        Point.prototype.add = function (point) {
+            return new Point(this.x + point.x, this.y + point.y);
+        };
+        Point.prototype.toString = function () {
+            return this.x + " " + this.y;
+        };
+        return Point;
+    }());
+    var p1 = new Point(10, 30);
+    var p2 = new Point(3, 9);
+    var p3 = p1.add(p2);
+    document.write(p3.toString());
+});
--- js/app.ts
+++ js/app.ts
@@ -1,16 +1,30 @@
-class Point {
-  x: number;
-  y: number;
+(function() {
+  'use strict';
 
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+  class Point {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+    }
+
+    add(point: Point) : Point {
+      return new Point(
+        this.x + point.x,
+        this.y + point.y
+      );
+    }
+
+    toString() : string {
+      return this.x + " " + this.y;
+    }
   }
 
-  add(point: Point) {
-    return new Point(
-      this.x = Point.x,
-      this.y = Point.y
-    );
-  }
-}
+  let p1 = new Point(10, 30);
+  let p2 = new Point(3 , 9 );
+  let p3 = p1.add(p2);
+
+  document.write(p3.toString());
+});
